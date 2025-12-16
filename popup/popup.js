5b19437279c546de4e3c801f0b97c6884d82ec29
Abdalla <alhagabdalla@gmail.com>
2025-12-16 11:48:06 -0800

    Adjusted some of the popup shortcuts to work properly and refactored code with global count

--- popup/popup.js
+++ popup/popup.js
@@ -3,60 +3,61 @@ let dec = document.querySelector("#dec");
 let reset = document.querySelector("#reset");
 let count = document.querySelector("#count");
 
-chrome.storage.sync.get("count", (data) => {
+let currentCount = 0;
+
+function render() {
+  count.textContent = currentCount;
+}
+function save() {
+  chrome.storage.local.set({ count: currentCount });
+}
+
+chrome.storage.local.get("count", (data) => {
   console.log(data, ", initial");
-  if (data.count) {
-    count.textContent = `${data.count}`;
-  } else {
-    saveToStorage(0);
-    count.textContent = "0";
+  if ("count" in data) {
+    currentCount = data.count;
   }
+  render();
 });
 
-function saveToStorage(value) {
-  chrome.storage.sync.set({ count: value });
-}
 document.body.tabIndex = -1;
 document.body.focus();
 
 document.addEventListener("keydown", (e) => {
-  if (e.code === "Space") {
+  if (["Space", "ArrowUp", "ArrowRight", "KeyK"].includes(e.code)) {
     e.preventDefault();
     e.stopPropagation();
-    incrementTally();
+    currentCount++;
   }
-  if (e.code === "ArrowUp" || e.code === "ArrowRight") {
+
+  if (["ArrowDown", "ArrowLeft", "KeyJ"].includes(e.code)) {
     e.preventDefault();
-    e.stopPropagation();
-    incrementTally();
+    currentCount--;
   }
-  if (e.code === "ArrowDown" || e.code === "ArrowLeft") {
+  if (e.code === "KeyR") {
     e.preventDefault();
-    e.stopPropagation();
-    decrementTally();
+    currentCount = 0;
   }
   if (e.code === "Escape") {
     e.preventDefault();
-    e.stopPropagation();
-    resetTally();
+    window.close();
   }
+  render();
+  save();
 });
 
-inc.addEventListener("click", incrementTally);
-dec.addEventListener("click", decrementTally);
-reset.addEventListener("click", resetTally);
-
-function incrementTally() {
-  count.textContent = `${parseInt(count.textContent) + 1}`;
-  saveToStorage(parseInt(count.textContent));
-}
-
-function decrementTally() {
-  count.textContent = `${parseInt(count.textContent) - 1}`;
-  saveToStorage(parseInt(count.textContent));
-}
-
-function resetTally() {
-  count.textContent = "0";
-  saveToStorage(parseInt(count.textContent));
-}
+inc.onclick = () => {
+  currentCount++;
+  render();
+  save();
+};
+dec.onclick = () => {
+  currentCount--;
+  render();
+  save();
+};
+reset.onclick = () => {
+  currentCount = 0;
+  render();
+  save();
+};
